762c74d414cb656f67e0aeb59ac7986180451194
brenman60 <83514481+brenman60@users.noreply.github.com>
2025-04-02 08:38:13 -0400

    Information changes

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -1,12 +1,16 @@
 const Award = ({
     award
 }) => {
+  const editedDescription = award.description.split("\n").map((line, index) => 
+    <span key={index}>{line}<br /></span>
+  );
+
   return (
     <>
       <h1 className="awardTitle">{award.title}</h1>
       <h2 className="awardPlacement" style={{ display: award.placement == "" ? "none" : ""}}>{award.placement}</h2>
       <h3 className="awardDate">{award.date}</h3>
-      <p className="awardDescription">{award.description}</p>
+      <p className="awardDescription">{editedDescription}</p>
     </>
   )
 };
